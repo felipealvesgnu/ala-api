52828dbae05e26ce4846d52ca37d0aab79de42f3
Felipe A L Pereira <felipealves.gnu@gmail.com>
2020-05-11 21:54:11 -0400

    Adding CORS config

--- src/main/java/br/org/ala/api/AlaApiApplication.java
+++ src/main/java/br/org/ala/api/AlaApiApplication.java
@@ -5,13 +5,25 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AlaApiApplication {
 
-  public static void main(String[] args) {
-    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-    SpringApplication.run(AlaApiApplication.class, args);
-  }
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(AlaApiApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
