fad99cd7c75bd0c2244e7da5475ec30175c82529
Felipe A L Pereira <felipealves.gnu@gmail.com>
2020-07-25 17:56:10 -0400

    Github actions

--- src/main/java/br/org/ala/api/AlaApiApplication.java
+++ src/main/java/br/org/ala/api/AlaApiApplication.java
@@ -2,7 +2,10 @@ package br.org.ala.api;
 
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
